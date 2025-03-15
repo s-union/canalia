d6dbd562848c60dbda6de791a4dde07cc19e2bf1
Hayato Sakaguchi <hayato8190+univ@gmail.com>
2025-03-15 22:10:58 +0900

    OpenAPIクライアントの設定とスクリプトの追加

---
+++ js/apps/next-app/src/lib/fetch.ts
@@ -0,0 +1,6 @@
+import createClient from 'openapi-fetch';
+import type { paths } from '../../generated/schema';
+
+export const client = createClient<paths>({
+  baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
+});
