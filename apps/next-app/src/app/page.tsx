f884baf4c75385e3478e13a7e2dd8ff3f016af4b
Hayato Sakaguchi <hayato8190+univ@gmail.com>
2025-04-03 15:07:24 +0900

    openapi-fetchからのバックエンドアクセス

--- apps/next-app/src/app/page.tsx
+++ apps/next-app/src/app/page.tsx
@@ -1,23 +1,33 @@
 import { auth0 } from '@/lib/auth0';
+import client from '@/lib/fetch';
 
 export default async function Home() {
 	const session = await auth0.getSession();
 
-	if (!session) {
+	if (session) {
+		const { data } = await client.GET('/user');
+
 		return (
 			<main>
-				<a href="/auth/login?screen_hint=signup">Sign up</a>
-				<a href="/auth/login">Log in</a>
+				<h1>Welcome, {session.user.name}!</h1>
+				<div>Access Token: {session.tokenSet.accessToken}</div>
+				<div>ID Token: {session.tokenSet.idToken}</div>
+				<a href="/auth/logout">Log out</a>
+				<h2>API Data</h2>
+				<div>name: {data?.name}</div>
+				<div>email: {data?.email}</div>
 			</main>
 		);
 	}
 
 	return (
 		<main>
-			<h1>Welcome, {session.user.name}!</h1>
-			<div>Access Token: {session.tokenSet.accessToken}</div>
-			<div>ID Token: {session.tokenSet.idToken}</div>
-			<a href="/auth/logout">Log out</a>
+			<div>
+				<a href="/auth/login?screen_hint=signup">Sign up</a>
+			</div>
+			<div>
+				<a href="/auth/login">Log in</a>
+			</div>
 		</main>
 	);
 }
