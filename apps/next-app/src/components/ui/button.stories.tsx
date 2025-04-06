273cf1e2d889247b0b367293044084b89c2f20cf
hayatosc <145091553+hayatosc@users.noreply.github.com>
2025-04-06 14:56:43 +0900

    ボタンコンポーネントの作成 (#15)
    * ボタンの基本となる部分を作成
    
    * 他パターンも追加
    
    * fix chromatic.yml
    
    * 指摘された点を反映
    
    * chromaticワークフローのパスを修正

---
+++ apps/next-app/src/components/ui/button.stories.tsx
@@ -0,0 +1,148 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+
+import { Upload } from 'lucide-react';
+
+import { Button } from './button';
+
+const meta: Meta<typeof Button> = {
+	title: 'Components/UI/Button',
+	component: Button,
+	tags: ['autodocs'],
+	parameters: {
+		layout: 'centered',
+	},
+	argTypes: {
+		onClick: { action: 'clicked' },
+		variant: {
+			control: 'select',
+			description: 'The variant of the button',
+			options: ['primary', 'secondary', 'error', 'success', 'outline', 'ghost'],
+		},
+		size: {
+			control: 'select',
+			description: 'The size of the button',
+			options: ['default', 'lg', 'icon'],
+		},
+		disabled: {
+			control: 'boolean',
+			description: 'If the button is disabled',
+		},
+		children: {
+			control: 'text',
+			description: 'The content of the button',
+		},
+		className: {
+			control: 'text',
+			description: 'Custom tailwind CSS classes to apply to the button',
+		},
+	},
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const DefaultButton: Story = {
+	args: {
+		variant: 'primary',
+		size: 'default',
+		disabled: false,
+		onClick: fn(),
+		children: 'Upload',
+	},
+};
+
+export const SecondaryButton: Story = {
+	args: {
+		variant: 'secondary',
+		size: 'default',
+		disabled: false,
+		onClick: fn(),
+		children: 'Upload',
+	},
+};
+
+export const ErrorButton: Story = {
+	args: {
+		variant: 'error',
+		size: 'default',
+		disabled: false,
+		onClick: fn(),
+		children: 'Delete',
+	},
+};
+
+export const SuccessButton: Story = {
+	args: {
+		variant: 'success',
+		size: 'default',
+		disabled: false,
+		onClick: fn(),
+		children: 'Save',
+	},
+};
+
+export const OutlineButton: Story = {
+	args: {
+		variant: 'outline',
+		size: 'default',
+		disabled: false,
+		onClick: fn(),
+		children: 'Upload',
+	},
+};
+
+export const GhostButton: Story = {
+	args: {
+		variant: 'ghost',
+		size: 'default',
+		disabled: false,
+		onClick: fn(),
+		children: 'Upload',
+	},
+};
+
+export const DisabledButton: Story = {
+	args: {
+		variant: 'primary',
+		size: 'default',
+		disabled: true,
+		onClick: fn(),
+		children: 'Disabled',
+	},
+};
+
+export const LargeButton: Story = {
+	args: {
+		variant: 'primary',
+		size: 'lg',
+		disabled: false,
+		onClick: fn(),
+		children: 'Upload',
+	},
+};
+
+export const WithIconButton: Story = {
+	args: {
+		variant: 'primary',
+		size: 'default',
+		disabled: false,
+		onClick: fn(),
+		children: (
+			<>
+				<Upload /> Upload
+			</>
+		),
+	},
+};
+
+export const IconButton: Story = {
+	args: {
+		variant: 'primary',
+		size: 'icon',
+		disabled: false,
+		onClick: fn(),
+		children: <Upload />,
+	},
+};
