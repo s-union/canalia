3dd90f68c42e07e1e7d56b6e522729742ca0686b
Hayato Sakaguchi <hayato8190+univ@gmail.com>
2025-04-10 00:05:25 +0900

    inputのスタイル更新

---
+++ apps/next-app/src/components/ui/input.stories.tsx
@@ -0,0 +1,58 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Input } from './input';
+
+const meta: Meta<typeof Input> = {
+	title: 'Components/UI/Input',
+	component: Input,
+	tags: ['autodocs'],
+	parameters: {
+		layout: 'centered',
+	},
+	argTypes: {
+		type: {
+			control: 'select',
+			description: '入力フィールドのタイプ',
+			options: [
+				'text',
+				'email',
+				'password',
+				'number',
+				'search',
+				'tel',
+				'url',
+				'date',
+			],
+		},
+		placeholder: {
+			control: 'text',
+			description: 'プレースホルダー',
+		},
+		disabled: {
+			control: 'boolean',
+			description: 'disabled属性',
+		},
+		className: {
+			control: 'text',
+			description: 'カスタムクラス',
+		},
+	},
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const DefaultInput: Story = {
+	args: {
+		type: 'text',
+		placeholder: 'input',
+	},
+};
+
+export const DisabledInput: Story = {
+	args: {
+		type: 'text',
+		placeholder: 'input',
+		disabled: true,
+	},
+};
