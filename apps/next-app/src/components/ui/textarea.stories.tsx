95a51815bbca8cbdc2977ead710914a719e9c2a1
hayatosc <145091553+hayatosc@users.noreply.github.com>
2025-04-11 10:15:33 +0900

    Feat/component/inputとtextarea (#21)
    * inputとtextareaのデフォルトスタイルを追加
    
    * textareaのスタイル更新
    
    * inputのスタイル更新
    
    * fileinputを追加

---
+++ apps/next-app/src/components/ui/textarea.stories.tsx
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Textarea } from './textarea';
+
+const meta: Meta<typeof Textarea> = {
+	title: 'Components/UI/Textarea',
+	component: Textarea,
+	tags: ['autodocs'],
+	parameters: {
+		layout: 'centered',
+	},
+	argTypes: {
+		placeholder: {
+			control: 'text',
+			description: 'プレースホルダー',
+		},
+		disabled: {
+			control: 'boolean',
+			description: 'disabled要素',
+		},
+		className: {
+			control: 'text',
+			description: 'カスタムクラス名',
+		},
+	},
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const DefaultTextarea: Story = {
+	args: {
+		placeholder: 'textarea',
+		className: 'w-96',
+	},
+};
+
+export const DisabledTextarea: Story = {
+	args: {
+		placeholder: 'textarea',
+		className: 'w-96',
+		disabled: true,
+	},
+};
